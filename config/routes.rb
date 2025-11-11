f758aee148b82b3c7bc3ca4cc22cc207cac52abf
RaduAndreiTaica <andrei.taica@takeofflabs.com>
2025-11-11 21:30:56 +0200

    update tip + skip csrf

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   get 'session/:session_token' => 'bill_sessions#show_session', as: :show_bill_session
   post 'validate_qr_token' => 'bill_sessions#validate_qr_token', as: :validate_qr_token
   
+  # Endpoints pentru bills (necesită sesiune validă)
+  get 'bills' => 'bills#show', as: :show_bill
+  patch 'bills/update_tip' => 'bills#update_tip', as: :update_bill_tip
+  
   namespace :users do
     #PAYMENTS
     post 'payment/create_payment' => 'payments#create_payment'
